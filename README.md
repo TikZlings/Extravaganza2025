ce6510215fff0530e2da83a5e989b3807d1a9c08
samcarter <sam.carter8@aol.com>
2024-12-14 12:38:18 +0100

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# The Great TikZlings Christmas Extravaganza 2025
+
+This repository contains the souce code of the Great TikZlings Extravanganza 2025, brought to you by [Ulrike](https://github.com/u-fischer) & [Gert Fischer](https://github.com/g-fischer), [Carla Maggi](https://github.com/CarLaTeX), [Paulo Cereda](https://github.com/cereda) and [samcarter](https://github.com/samcarter).
+
+The full video is available from ...
+
+## 1. Example
+
+<img align="right" src="./examplescene/example.png" height="100">
+
+- Music: 
+  
+- Image: 
+  
+- Source Code: [./examplescene/](https://github.com/TikZlings/Extravaganza2025/tree/main/examplescene)
---
+++ intermissions/convert2video.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# passing a title to the intermission
+# Params:
+# 1: jobname
+# 2: some string for the title
+convert2video(){
+
+    # compiling
+    latexmk -jobname=extravaganza-intermission-$1 -pretex="\def\mytitle{$2}" -usepretex extravaganza-intermission
+    
+    # clean up old video
+    rm $1.mp4
+
+    # convert to png images
+    # convert -density 160 extravaganza-intermission-$1.pdf $1.png
+    pdftoppm -png -r 240 -progress extravaganza-intermission-$1.pdf $1
+
+    # convert to video
+    ffmpeg -r 10 -ss 00:00:00 -i $1-%02d.png -ss 00:00:00 -i christmas-music-box-music.m4a -shortest $1_raw.mp4
+
+    # repair video
+    HandBrakeCLI --crop 0:0:0:0 -i $1_raw.mp4 -o $1.mp4
+
+    # clean up
+    rm $1-*.png
+    rm $1_raw.mp4
+    rm extravaganza-intermission-$1.aux
+    rm extravaganza-intermission-$1.log
+    rm extravaganza-intermission-$1.nav
+    rm extravaganza-intermission-$1.snm
+    rm extravaganza-intermission-$1.toc
+    rm extravaganza-intermission-$1.out
+    rm extravaganza-intermission-$1.fls
+    rm extravaganza-intermission-$1.fdb_latexmk
+
+}
+
+# repeat an existing pdf several times
+# Params:
+# 1: jobname
+# 2: path and name to the pdf file
+# 3: number of repetitions [default 75]
+repeatconvert(){
+
+    # compiling
+    latexmk -jobname=extravaganza-intermission-$1 -pretex="\def\filename{$2}\def\myrepeat{$3}" -usepretex repeat  
+    
+    # clean up old video
+    rm $1.mp4
+
+    # convert to png images
+    pdftoppm -png -r 240 -progress extravaganza-intermission-$1.pdf $1
+
+    # convert to video
+    ffmpeg -r 10 -ss 00:00:00 -i $1-%03d.png -ss 00:00:00 -i christmas-music-box-music.m4a -shortest $1_raw.mp4
+
+    # repair video
+    HandBrakeCLI --crop 0:0:0:0 -i $1_raw.mp4 -o $1.mp4
+
+    # clean up
+    rm $1-*.png
+    rm $1_raw.mp4
+    rm extravaganza-intermission-$1.aux
+    rm extravaganza-intermission-$1.log
+    rm extravaganza-intermission-$1.nav
+    rm extravaganza-intermission-$1.snm
+    rm extravaganza-intermission-$1.toc
+    rm extravaganza-intermission-$1.out
+    rm extravaganza-intermission-$1.fls
+    rm extravaganza-intermission-$1.fdb_latexmk
+        
+}
+
+# preview images
+pdftoppm -png -r 48 -progress -singlefile extravaganza-titlepage.pdf icon_small
+pdftoppm -png -r 100 -progress -singlefile extravaganza-titlepage.pdf icon_medium
+pdftoppm -png -r 150 -progress -singlefile extravaganza-titlepage.pdf icon_large
+
+# Converting
+repeatconvert title "./extravaganza-titlepage.pdf" 101
+convert2video example "Example"
+repeatconvert credits "./extravaganza-credits.pdf" 150
