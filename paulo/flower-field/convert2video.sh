a4a070e7f079bce66b66b3ee07f773ebd5d84905
samcarter <sam.carter8@aol.com>
2025-11-20 22:40:48 +0100

    flower-field: convert to video

--- paulo/flower-field/convert2video.sh
+++ paulo/flower-field/convert2video.sh
@@ -2,13 +2,13 @@
 rm flower-field.mp4
 
 # convert to png images
-pdftoppm -png -r 48 -progress -singlefile -f 10 flower-field.pdf flower-field
+pdftoppm -png -r 48 -progress -singlefile -f 10 flower-field_repeat.pdf flower-field
 pdftoppm -png -r 240 -progress flower-field.pdf flower-field
 
 # convert to video
 ffmpeg \
   -ss 00:00:00 -i flower-field-%03d.png \
-  -ss 00:00:10 -i Music.m4a \
+  -ss 00:00:08 -i Where_Have_All_The_Flowers_Gone_-_With_Lyrics_-_Pete_Seeger-MR_BING_DIY-JANHG_OYbso.m4a \
   -shortest flower-field_raw.mp4
 
 # repair video
