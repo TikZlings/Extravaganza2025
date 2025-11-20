e42873b4126d4abf70b8c1d97133c776d1748f0a
samcarter <sam.carter8@aol.com>
2025-11-20 17:33:42 +0100

    flower-field: adding video script

---
+++ paulo/flower-field/convert2video.sh
@@ -0,0 +1,21 @@
+# clean up old video
+rm flower-field.mp4
+
+# convert to png images
+pdftoppm -png -r 48 -progress -singlefile -f 10 flower-field.pdf flower-field
+pdftoppm -png -r 240 -progress flower-field.pdf flower-field
+
+# convert to video
+ffmpeg \
+  -ss 00:00:00 -i flower-field-%03d.png \
+  -ss 00:00:10 -i Music.m4a \
+  -shortest flower-field_raw.mp4
+
+# repair video
+HandBrakeCLI --crop 0:0:0:0  -i flower-field_raw.mp4 -o flower-field.mp4
+
+# clean up
+if [ -e "flower-field.mp4" ]; then
+  rm flower-field-*.png
+  rm flower-field_raw.mp4
+fi
