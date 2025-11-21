554a1b65f8a97760966c0ada2abf09396982ef1e
samcarter <sam.carter8@aol.com>
2025-11-21 09:51:10 +0100

    flower-field: full speed video

--- paulo/flower-field/convert2video.sh
+++ paulo/flower-field/convert2video.sh
@@ -2,8 +2,8 @@
 rm flower-field.mp4
 
 # convert to png images
-pdftoppm -png -r 48 -progress -singlefile -f 10 flower-field_repeat.pdf flower-field
-pdftoppm -png -r 240 -progress flower-field_repeat.pdf flower-field
+pdftoppm -png -r 48 -progress -singlefile -f 10 flower-field.pdf flower-field
+pdftoppm -png -r 240 -progress flower-field.pdf flower-field
 
 # convert to video
 ffmpeg \
